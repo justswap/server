b14a5ff169c086aee17809e9a1b019b744f419ee
Jakub Semik <kuba.semik@gmail.com>
2018-08-11 11:09:23 +0200

    fixed spacing

--- server/docker/entrypoint-test.sh
+++ server/docker/entrypoint-test.sh
@@ -5,4 +5,4 @@ python wait_redis_postgres.py
 python manage.py migrate
 
 touch /tmp/.done.info
-    daphne -b 0.0.0.0 -p 8001 config.asgi:application
+daphne -b 0.0.0.0 -p 8001 config.asgi:application
